3805408052c57b6d467efc30fb38390a2503c845
Man Yang <manyang1989@gmail.com>
2016-11-07 17:56:28 +0800

    add get method

--- components/Favlist.vue
+++ components/Favlist.vue
@@ -6,8 +6,24 @@
 export default {
   data () {
     return {
-      msg: 'Hello from vue-loader!'
+      msg: '123'
     }
+  },
+  methods:{
+  	 greeting(){
+  	 	var xhr = new XMLHttpRequest();
+  	 	this.msg=456;
+    	xhr.open('GET', 'http://localhost:8888/user', false);
+    	xhr.setRequestHeader("Content-Type", "text/plain");
+    	xhr.onreadystatechange = function () {
+    	    if (xhr.readyState == 4) {
+    	        if (xhr.status == 200) {
+    	            this.msg = xhr.responseText;
+    	        }
+    	    }
+    	};
+    	xhr.send(null);
+  	}
   }
 }
 </script>
